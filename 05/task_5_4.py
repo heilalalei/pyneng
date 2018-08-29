a639d554e6bc0865ebe080ba99ca71a1eded940e
heilalalei <marsel-582@yandex.ru>
2018-08-29 00:57:56 -0400

    Решил задачу 5.4

---
+++ 05/task_5_4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+num_list = [10, 2, 30, 100, 10, 50, 11, 30, 15, 7]
+word_list = [
+    'python', 'ruby', 'perl', 'ruby', 'perl', 'python', 'ruby', 'perl'
+]
+
+num = input('Enter a number from num_list:\n')
+print(9-num_list[::-1].index(int(num)))
+word = input('Enter a word from word_list:\n')
+print(7-word_list[::-1].index(word))
+
+
+
