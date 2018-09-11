4418fd80f9177d2bb3e095358b2c779190afba40
heilalalei <marsel-582@yandex.ru>
2018-09-11 01:53:54 -0400

    6.1a v2

--- 06/task_6_1a.py
+++ 06/task_6_1a.py
@@ -1,7 +1,55 @@
 #!/usr/bin/env python3
+a = True
+b = False
+ipaddr = input("Введите IP-адрес:\n")
+ip = ipaddr.split('.')
+while a==True:
+    if len(ip) != 4:
+        print('wrong')
+        a = False
+        break
+    for i in range(4):
+        if not ip[i].isdigit():
+            print('wrong2')
+            a = False
+            break
+    if a == False:
+        break
+    for i in range(4):
+        if not (int(ip[i]) <= 255 and int(ip[i]) >= 0):
+            print('wrong3')
+            a = False
+            break
+        else:
+            b = True
+            continue
+    if b == False:
+        break
+    else:
+        if ipaddr == '0.0.0.0':
+            print('unassigned')
+        elif ipaddr == '255.255.255.255':
+            print('local broadcast')
+        elif int(ip[0]) > 0 and int(ip[0]) <= 223:
+            print('unicast')
+        elif int(ip[0]) >= 224 and int(ip[0]) <= 239:
+            print('multicast')
+        else:
+            print('unused')
+
+
+
+
+
+
+
+
+
+
+'''
 
 ipaddr = input("Введите IP-адрес:\n") 
-ip = [ipaddr.split('.')[i] for i in range(4)]
+ip = [ipaddr.split('.')
 if len(ipaddr.split('.')) != 4:
     print('Incorrect address')
 else:
@@ -21,4 +69,4 @@ else:
             else:
                 print('unused')
  
-
+'''
