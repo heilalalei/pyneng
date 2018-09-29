c6d47a9f61546aeef98c28cfdac21b34e9a9f9ae
heilalalei <marsel-582@yandex.ru>
2018-09-28 23:38:16 -0400

    Таки не решил задачи 7.3a и 7.3b

--- 07/task_7_3a.py
+++ 07/task_7_3a.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 #Задача 7.3a
+#ПОЧЕМУ Я НЕ МОГУ ПРОПУСТИТЬ ПУСТУЮ СТРОЧКУ?!?!?!?!?
+#Не решил эту задачу
 f = open('CAM_table.txt')
 vlans = []
 for line in f:
-    vlans.append(line.split()[0])
-    for word in line.split():
-        if '.' in word:
-            print(line[:len(line)-1].replace(' DYNAMIC ',''))
+    if line.split()[0].isdigit():
+        vlans.append(line.split()[0])
+        for word in line.split():
+            if '.' in word:
+                print(line[:len(line)-1].replace(' DYNAMIC ',''))
 print(vlans)
 
